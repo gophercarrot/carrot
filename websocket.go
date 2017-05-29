6940b3daec70f7fe743d04665099513d244ad24f
AbhishekGahlot <me@abhishek.it>
2017-05-29 14:12:34 +0530

    create ws instance

--- websocket.go
+++ websocket.go
@@ -1,9 +1,36 @@
 package carrot
 
 import (
-	"godoc.org/github.com/gorilla/websocket"
+	"flag"
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
 )
 
-func Rand(){
-	return 4
+//unused for later purpose
+type completion struct {
+	body struct {
+		code           string
+		fileType       string
+		line           int
+		column         int
+		wordToComplete string
+		offset         int
+	}
+}
+
+func getAddr(addr string) *string {
+	return flag.String("addr", addr, "http service address")
+}
+
+// CreateSocket returns a socket instance
+func CreateSocket(addr string) *websocket.Conn {
+	wsaddr := url.URL{Scheme: "wss", Host: *getAddr(addr), Path: "/"}
+
+	c, _, err := websocket.DefaultDialer.Dial(wsaddr.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	return c
 }
