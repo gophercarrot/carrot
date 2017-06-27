ce87b23036bc75042fd33d644806905ced7d5db2
rishiloyola <rishiloyola98245@gmail.com>
2017-06-27 16:13:52 +0530

    Add command line args
    Add command line args
    
    Add command line args

--- README.md
+++ README.md
@@ -27,6 +27,14 @@ Go to $GOPATH/go/src/github.com/interviewstreet/carrot and run
 go run cmd/main.go
 ```
 
+## Config at Run time
+```
+go run main.go -host=example.com -protocol=wss -htime=40 -request=5000 -wtime=1 -htime=30 -path=/somepath
+
+wtime -> number of seconds to wait before writing to websockets
+htime -> number of milliseconds to wait before creating new websocket connection
+```
+
 ## Parameters in main.go
 ```	
 currentTest := &carrot.Base{"example.com", "wss", 1000, msg, 2, 30}
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"runtime"
 	"time"
-	
+
 	"github.com/interviewstreet/carrot"
 )
 
@@ -13,11 +14,31 @@ var count = 1000
 var httpPort = 8900
 
 func main() {
+
+	var host string
+	flag.StringVar(&host, "host", "example.com", "Domain name of test host")
+
+	var protocol string
+	flag.StringVar(&protocol, "protocol", "wss", "Connection type")
+
+	var request int
+	flag.IntVar(&request, "request", 6000, "Total number of requests")
+
+	var writeTime int
+	flag.IntVar(&writeTime, "wtime", 100, "number of seconds to wait before writing to websockets")
+
+	var holdTime int
+	flag.IntVar(&holdTime, "htime", 30, "number of milliseconds to wait before creating new websocket connection")
+
+	var path string
+	flag.StringVar(&path, "path", "/somepath", "Specific url path")
+
+	flag.Parse()
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	latency := make(chan []float64)
 	timeSeries := make(chan []time.Time)
-
-	currentTest := &carrot.Base{"example.com", "wss", 6000, msg, 100, 30, "/somepath"}
+	currentTest := &carrot.Base{host, protocol, request, msg, writeTime, holdTime, path}
 	carrot.LoadTest(currentTest, latency, timeSeries)
 
 	data := <-latency
